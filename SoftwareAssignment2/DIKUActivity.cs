04a09276beeff4c1825b7c6d957877581183c151
Christopher Lind <Christopher.lind.c@gmail.com>
2021-02-14 10:18:42 +0100

    Created base DIKUperson Created enum DIKUActivity

---
+++ SoftwareAssignment2/DIKUActivity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareAssignment2
+{
+    enum DIKUActivity
+    {
+        AttendingAll,
+        AttendingSome,
+        AttendingNone,
+        _,
+    }
+}
---
+++ SoftwareAssignment2/DIKUperson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareAssignment2
+{
+    class DIKUperson
+    {
+        string name;
+        int maxBrainPower;
+        int brainPower;
+        int strengthOfArgument;
+        int semester;
+        int counterArgument;
+        int doubleArgument;
+        Activity activity;
+
+    }
+}
