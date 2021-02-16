4e4aeeb66c65463d06810aad99733a2dcaa9e2bf
Christopher Lind <Christopher.lind.c@gmail.com>
2021-02-16 11:09:37 +0100

    Added fancy console writer

---
+++ SoftwareAssignment2/FancyConsoleText.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareAssignment2
+{
+    static class FancyConsoleText
+    {
+        const int waitTimeMS = 3; // wait time between each animation loop
+        const int padLength = 20; // from how far the new letter should come
+
+        public static void FromLeft(string text)
+        {
+            string finishedText = "";
+            foreach (char c in text)
+            {
+                AnimateCharFromleft(finishedText, c, text.Length);
+                finishedText += c;
+            }
+        }
+
+        private static string PadTextRight(string finishedText, char c, int charPad, int fullPad)
+        {
+            StringBuilder padder = new StringBuilder();
+            padder.Append(finishedText.PadRight(charPad));
+            padder.Append(c);
+            padder.Length += fullPad - charPad;
+            return padder.ToString();
+        }
+
+        private static void AnimateCharFromleft(string finishedText, char c, int fullStringLength)
+        {
+            string printText;
+            int fullPadding = padLength + fullStringLength;
+            for (int i = fullPadding; i >= 0; i--)
+            {
+                printText = PadTextRight(finishedText, c, i, fullPadding);
+                ClearLastLine();
+                Console.Write(printText);
+                System.Threading.Thread.Sleep(waitTimeMS);
+            }
+        }
+
+        public static void ClearLastLine()
+        {
+            Console.Write("\r");
+        }
+    }
+}
--- SoftwareAssignment2/Program.cs
+++ SoftwareAssignment2/Program.cs
@@ -7,24 +7,28 @@ namespace SoftwareAssignment2
     {
         static void Main(string[] args)
         {
-            List<DIKUPerson> studentList = new List<DIKUPerson>
-            {
-                new DIKUStudent("John Doe", DIKUActivity.AttendingAll),
-                new DIKUStudent("Jane Doe", DIKUActivity.AttendingNone),
-                new DIKUStudent("Frank Buck", DIKUActivity.AttendingSome),
-                //new DIKUStudent("Hireath Under", DIKUActivity.AttendingAll),
-                //new DIKUStudent("Lacey Cuypers", DIKUActivity.AttendingAll),
-                //new DIKUStudent("Fortune Marlowe", DIKUActivity.AttendingNone),
-                //new DIKUStudent("Diana Peeters", DIKUActivity.AttendingSome),
-                //new DIKUStudent("Jonas Alvin", DIKUActivity.AttendingAll),
-                //new DIKUStudent("Samule Milford", DIKUActivity.AttendingAll),
-                //new DIKUStudent("Carina Steffensen", DIKUActivity.AttendingNone),
-                //new DIKUStudent("Vance Hüber", DIKUActivity.AttendingSome),
-                //new DIKUStudent("Tobias Henning", DIKUActivity.AttendingAll),
-            };
+            //List<DIKUPerson> studentList = new List<DIKUPerson>
+            //{
+            //    new DIKUStudent("John Doe", DIKUActivity.AttendingAll),
+            //    new DIKUStudent("Jane Doe", DIKUActivity.AttendingNone),
+            //    new DIKUStudent("Frank Buck", DIKUActivity.AttendingSome),
+            //    //new DIKUStudent("Hireath Under", DIKUActivity.AttendingAll),
+            //    //new DIKUStudent("Lacey Cuypers", DIKUActivity.AttendingAll),
+            //    //new DIKUStudent("Fortune Marlowe", DIKUActivity.AttendingNone),
+            //    //new DIKUStudent("Diana Peeters", DIKUActivity.AttendingSome),
+            //    //new DIKUStudent("Jonas Alvin", DIKUActivity.AttendingAll),
+            //    //new DIKUStudent("Samule Milford", DIKUActivity.AttendingAll),
+            //    //new DIKUStudent("Carina Steffensen", DIKUActivity.AttendingNone),
+            //    //new DIKUStudent("Vance Hüber", DIKUActivity.AttendingSome),
+            //    //new DIKUStudent("Tobias Henning", DIKUActivity.AttendingAll),
+            //};
 
-            Classroom storeKnirke = new Classroom();
-            storeKnirke.RunDebate(studentList);
+            //Classroom storeKnirke = new Classroom();
+            //storeKnirke.RunDebate(studentList);
+
+            string testString = "testString";
+
+            FancyConsoleText.FromLeft(testString);
 
 
         }
